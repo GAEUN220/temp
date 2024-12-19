a0d0bbd50613ee65e39abda466bb6f183bf5624b
gaeun220 <gaeun2701@gmail.com>
2024-12-19 16:42:29 +0900

    doWrite -> write

--- src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -11,7 +11,7 @@ public class PostController {
     @ResponseBody
     public String showWrite() {
         return """
-                <form action="/posts/doWrite" method="POST">
+                <form action="/posts/write" method="POST">
                     <input type="text" name="title" placeholder="제목">
                     <textarea name="content" placeholder="내용"></textarea>
                     <button type="submit">글쓰기</button>
@@ -19,7 +19,7 @@ public class PostController {
                 """;
     }
 
-    @PostMapping("/doWrite")
+    @PostMapping("/write")
     @ResponseBody
     public String write(
             @RequestParam String title,
